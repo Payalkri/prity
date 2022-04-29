8a1cf69f4bba08a84084be1e32c9fdb0e4e8b48e
Payalkri <104563222+Payalkri@users.noreply.github.com>
2022-04-29 14:28:28 +0530

    imite commit

---
+++ practice 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Sample
+{
+    class Test
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("C# is cool");
+        }
+    }
+}
+
+    
+
